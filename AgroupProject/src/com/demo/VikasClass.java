3849a28c291cc30bfd63912442bddaa3f16cc7f9
Vikas1267 <indlavikas57@gmail.com>
2026-07-29 12:24:49 +0530

    PUSHH

--- AgroupProject/src/com/demo/VikasClass.java
+++ AgroupProject/src/com/demo/VikasClass.java
@@ -3,6 +3,8 @@ package com.demo;
 public class VikasClass {
 
 		public static void main(String[] args) {
-			System.out.println("VIkas by A   gROUP");
+			System.out.println("VIkas by A      gROUP");
+			System.out.println("VIkas by A      gROUP".contains("V"));
+			
 		}
 	}
