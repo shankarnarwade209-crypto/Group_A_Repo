4b4ccb4028dfd56a169ba18e336994e256329f51
Vikas1267 <indlavikas57@gmail.com>
2026-07-29 12:21:56 +0530

    PUSH

--- AgroupProject/src/com/demo/VikasClass.java
+++ AgroupProject/src/com/demo/VikasClass.java
@@ -3,6 +3,6 @@ package com.demo;
 public class VikasClass {
 
 		public static void main(String[] args) {
-			System.out.println("VIkas by A  gROUP");
+			System.out.println("VIkas by A   gROUP");
 		}
 	}
